689abe2ecb1c8b380040013da28fe49b4fc89206
Strayko <strayko_lol@windowslive.com>
2020-07-06 16:33:33 +0200

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DeskBooker.Core.Domain;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Moamer",
+                LastName = "Jusupovic",
+                Email = "moamer@live.com",
+                Date = new DateTime(2020, 07, 06)
+            };
+            var processor = new DeskBookingRequestProcessor();
+
+            // Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
---
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        public DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+    }
+}
